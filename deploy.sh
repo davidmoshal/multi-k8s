423a62072a9e644ee1bae9597aaf64765fd63255
davidmoshal <dmoshal@gmail.com>
2019-12-08 17:28:56 -0800

    Change usernames to mine.

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t davidmoshal/multi-client:latest -t davidmoshal/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t davidmoshal/multi-server:latest -t davidmoshal/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t davidmoshal/multi-worker:latest -t davidmoshal/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push davidmoshal/multi-client:latest
+docker push davidmoshal/multi-server:latest
+docker push davidmoshal/multi-worker:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push davidmoshal/multi-client:$SHA
+docker push davidmoshal/multi-server:$SHA
+docker push davidmoshal/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=davidmoshal/multi-server:$SHA
+kubectl set image deployments/client-deployment client=davidmoshal/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=davidmoshal/multi-worker:$SHA
